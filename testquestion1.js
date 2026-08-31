e6e6bd963dc4ee412cf5a77236a9f495c1bdb52b
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-31 10:01:53 +0000

    Aug 31, 2026, 10:01 AM

--- testquestion1.js
+++ testquestion1.js
@@ -8,7 +8,7 @@ function draw(){
     fill("black")
     stroke("red")
     strokeWeight(10)
-    triangle(240,200,250,320,350,320)
+    triangle(300,200,250,320,350,320)
     fill("black")
     stroke("red")
     strokeWeight(10)
