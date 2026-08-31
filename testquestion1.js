d9b5bbc7ce2891a6be605d312da501a223c38356
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-31 09:55:46 +0000

    Aug 31, 2026, 9:55 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,10 +1,10 @@
 function setup(){
     createCanvas(600,400);
-    fill("white")
+    
 
 }
 function draw(){
-    circle(400,400,130)
+    circle(400,200,130)
     fill("black")
     stroke("red")
     strokeWeight(10)
