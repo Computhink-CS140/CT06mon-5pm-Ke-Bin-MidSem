8b30232cc8799b100b3e2c8ba000116235a20ac7
Computhink-CS140 <cs140@computhink.com.sg>
2026-08-31 10:32:39 +0000

    Aug 31, 2026, 10:32 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,10 +1,10 @@
 function setup(){
     createCanvas(600,600);
-    background(0);
+    background(220);
 
 }
 function draw(){
     for (let count = 0; count < 10; count++) {
-        circle(50+count*25,50+i*25,25);
+        circle(50+count*25,50+count*25,25);
     }
 }
